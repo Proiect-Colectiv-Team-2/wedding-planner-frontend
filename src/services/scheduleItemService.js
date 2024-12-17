7317d5abffddb853706f5a0f2a17c7c2a54f32b6
vasiu <e12125518@student.tuwien.ac.at>
2024-12-17 23:22:48 +0200

    implement delete schedule item functionality

--- src/services/scheduleItemService.js
+++ src/services/scheduleItemService.js
@@ -7,3 +7,8 @@ export const getScheduleItems = async () => {
     const response = await api.get('/api/schedule-items');
     return response.data;
 };
+
+export const deleteScheduleItem = async (scheduleItemId) => {
+    const response = await api.delete(/api/schedule-items/${scheduleItemId});
+    return response.data;
+}
